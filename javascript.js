3f0e6ea538e0493de6b4e467c7b9c3f8d7da0280
Priya <shikhakamdee@gmail.com>
2024-01-30 09:10:59 +0530

    change mode

--- javascript.js
+++ javascript.js
@@ -5,3 +5,22 @@ var typed = new Typed(".text", {
   backDelay: 1000,
   loop: true,
 });
+
+const btn = document.getElementById("toggle");
+console.log(btn);
+const sunIcon = document.getElementsByClassName("fa-sun")[0];
+btn.addEventListener("click", () => {
+  console.log("button clicked");
+  var cssLink = document.getElementById("cssLink");
+
+  // Assuming you have two CSS files: style1.css and style2.css
+  if (cssLink.getAttribute("href") === "style.css") {
+    cssLink.setAttribute("href", "light-style.css");
+    sunIcon.classList.add("fa-moon");
+    sunIcon.classList.remove("fa-sun");
+  } else {
+    cssLink.setAttribute("href", "style.css");
+    sunIcon.classList.add("fa-sun");
+    sunIcon.classList.remove("fa-moon");
+  }
+});
